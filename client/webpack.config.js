f67ab8bbf5690af91a9dfbffd9223f388a854270
Nathan Patnaude <nathanpatnaude@gmail.com>
2023-03-05 23:12:55 -0500

    configured workbox plugins, added babel, added registerRoute in src-sw and in webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
+
 
 module.exports = () => {
   return {
@@ -18,12 +18,56 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //* TODO: Add and configure workbox plugins for a service worker and manifest file.
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: "Jousters Answer Tactical Expanse"
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Jousters Answer Tactical Expanse',
+        short_name: 'J.A.T.E.',
+        description: 'We shall solve the triangle!',
+        background_color: '#F5DA9C',
+        theme_color: '#F4E9B8',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
+      //* TODO: Add CSS loaders and babel to webpack.
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
